013fc8d5229b204d665b36f94da1c0f6d6047939
Nageswara <Nageswara.koppisetti@gmail.com>
2021-01-09 14:00:39 +0530

    WH-9090-First Time commit

---
+++ src/main/java/com/vihaan/springcloud/Runner/MessageRunner.java
@@ -0,0 +1,16 @@
+package com.vihaan.springcloud.Runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to spring boot app");
+
+	}
+
+}
